423776d6c05049f92519cf46a4b7a8a10a475e56
Shoyeb <shoyeb.inamdar@volkswagen.co.in>
2019-04-06 14:58:21 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/'));
 app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
-app.listen(process.env.PORT || 8080);
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function(){
+    console.log("yep", app.get('ip'), app.get('port'));
+});
